327132044446fde6171864e1e2f273d8b0ab58aa
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-23 12:16:53 +0530

    setted up an ingress controller to redirect request to different services inside the cluster and also created a get api endpoint handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ import {json} from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentUser" , (req,res) => {
+  res.send("hello from backend");
+})
+
 app.listen(3000 , () => {
   console.log("running auth on port 3000 !!!");
 })
