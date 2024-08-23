ded1f148fc0909a5e9a7b9844e5f7861d2ad427e
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-23 19:12:50 +0530

    added express-validator to make validation and also tested the signup route with postman

--- auth/src/routes/sigup.ts
+++ auth/src/routes/sigup.ts
@@ -1,9 +1,37 @@
-import express from "express";
+import express,{Request,Response} from "express";
+import { body , validationResult} from 'express-validator';
 
 const router = express.Router();
 
-router.get("/api/users/signup" , (req,res) => {
-  res.send("signup route called");
-})
+router.post("/api/users/signup" , 
+ [
+    body('email')
+    .isEmail()
+    .withMessage('Not a valid e-mail address') 
+    , 
+    body('password')
+    .trim()
+    .isLength({min:4 , max:10})
+    .withMessage('Password must be of length between 4 and 10')
+  ] 
+  , 
+  (req:Request,res:Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+      return res.status(400).send(errors.array());
+    }
+    
+    const {email , password } = req.body;
+
+    console.log("creating a user");
+
+    return res.status(200).json({
+      message : "user has been sucessfully created",
+      data : req.body,
+    });
+
+  }
+)
 
 export { router as signUpRoute };
