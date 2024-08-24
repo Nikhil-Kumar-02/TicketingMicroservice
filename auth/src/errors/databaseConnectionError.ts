b4d2b28372193a923d4c321331c2c54e38b03f7d
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-25 00:05:33 +0530

    created a cutom error abstract class to ensure that every error class has a same error sending body

--- auth/src/errors/databaseConnectionError.ts
+++ auth/src/errors/databaseConnectionError.ts
@@ -1,3 +1,5 @@
+import { CustomError } from "./customError";
+
 interface CustomErrorStructure{
   statusCode:number;
   serializeErrors():{
@@ -7,13 +9,24 @@ interface CustomErrorStructure{
   }[];
 }
 
+/**
+ * remainder on abstract class
+ * cannot be instanctiated
+ * used to setup requirements for the sub class
+ * do create a class when translated to js, which means we can use instance of checks!!
+ * but the interface fades away when converted to js
+ * the problem with interface is ki we have to check for every chlid ones instance
+ * as the parent inteface doesnot exits in js thats why using abstract class
+ */
 
-export class DatabaseConnectionError extends Error implements CustomErrorStructure{
+
+export class DatabaseConnectionError extends CustomError{
   statusCode = 500;
   reason = "Error connecting to database";
 
   constructor(){
-    super();
+    super("Error connecting to db");
+    //above message will not be sent to the end user rather they are only for logging purposes
 
     Object.setPrototypeOf(this , DatabaseConnectionError.prototype);
   }
