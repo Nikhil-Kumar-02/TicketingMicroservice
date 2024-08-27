ad3bf1f96ea452c23e5992a87b154b0031865ed6
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-27 13:12:13 +0530

    created the current-users api endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from "express";
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.get("/api/users/currentUser" , (req,res) => {
-  res.send("hello from backend");
+
+  if(!req.session?.jwt){
+    return res.send({currentUser: null});
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt , process.env.JWT_KEY!);
+    return res.send({currentUser: payload});
+  } catch (error) {
+    return res.send({currentUser : null});
+  }
+
 })
 
 export { router as currentUserRoute };
