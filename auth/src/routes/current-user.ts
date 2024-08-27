3f4f810018940d069b6abf90448868cbb1d455a7
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-27 16:26:15 +0530

    added things to pass the authentication check and created an error to return that kind of authorization

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import { currentUser } from "../middlewares/current-user";
 
+
 router.get("/api/users/currentUser" , currentUser , (req,res) => {
 
   return res.send({currentUser: req.currentUser||null});
