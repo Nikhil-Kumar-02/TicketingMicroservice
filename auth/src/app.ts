6a65c4cb9d3b6e0440eb4cdfa7245fa6870e41fb
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-08-28 19:15:08 +0530

    moved some code from index to app as this is what the testig with supertest and jest requires

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import 'express-async-errors';
+import {json} from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRoute } from "./routes/current-user";
+import { signInRoute } from "./routes/signin";
+import { signOutRoute } from "./routes/signout";
+import { signUpRoute } from "./routes/sigup";
+import { errorHandler } from "./middlewares/err-handler";
+import { NotFoundError } from "./errors/notFoundError";
+
+const app = express();
+app.set("trust proxy" , true);
+
+app.use(json());
+
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}))
+
+app.use(currentUserRoute);
+app.use(signInRoute);
+app.use(signOutRoute);
+app.use(signUpRoute);
+
+app.all('*' , ()=>{
+  throw new NotFoundError();
+})
+
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from "express";
-import 'express-async-errors';
-import {json} from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRoute } from "./routes/current-user";
-import { signInRoute } from "./routes/signin";
-import { signOutRoute } from "./routes/signout";
-import { signUpRoute } from "./routes/sigup";
-import { errorHandler } from "./middlewares/err-handler";
-import { NotFoundError } from "./errors/notFoundError";
-
-const app = express();
-app.set("trust proxy" , true);
-
-app.use(json());
-
-app.use(cookieSession({
-  signed: false,
-  secure: true,
-}))
-
-app.use(currentUserRoute);
-app.use(signInRoute);
-app.use(signOutRoute);
-app.use(signUpRoute);
-
-app.all('*' , ()=>{
-  throw new NotFoundError();
-})
-
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   try {
