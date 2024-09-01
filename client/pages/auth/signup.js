0c2628ab382bc57be5cd267cd35440baf2b0f2f4
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-09-01 07:45:30 +0530

    bootstrap is now correctly working and also improved error throwing situations

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,10 +1,23 @@
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
+
+  const emailError = errors
+    .filter(err => err.field === "email")
+    .map(err => <li key={err.message}>{err.message}</li>);
+  
+
+  const passwordError = errors
+    .filter(err => err.field === "password")
+    .map(err => <li key={err.message}>{err.message}</li>);
+  
+  console.log(emailError);
+  console.log(passwordError);
 
   const onSubmit = async event => {
     event.preventDefault();
@@ -31,6 +44,14 @@ export default () => {
           onChange={e => setEmail(e.target.value)}
           className="form-control"
         />
+        {emailError && emailError.length > 0 && (
+          <div className="alert alert-danger">
+            <h6>Ooops....</h6>
+            <ul className="my-0">
+              {emailError}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="form-group">
         <label>Password</label>
@@ -40,17 +61,15 @@ export default () => {
           type="password"
           className="form-control"
         />
+        {passwordError && passwordError.length > 0 && (
+          <div className="alert alert-danger">
+            <h6>Ooops....</h6>
+            <ul className="my-0">
+              {passwordError}
+            </ul>
+          </div>
+        )}
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-          <ul className="my-0">
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
