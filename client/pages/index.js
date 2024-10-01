518b280e771085d12bc72ad2967a8bfd9b9e10ba
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-10-01 13:33:26 +0530

    refactored the code to make request to backend either form the browser or from the server so we can use it again and again

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
+import buildClient from "../api/build-client";
 
-const LandingPage = ({ currentUser }) => {
+const LandingPage = (currentUser) => {
   // console.log(currentUser);
   // axios.get('/api/users/currentuser');
   console.log(currentUser);
@@ -8,27 +9,14 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
+LandingPage.getInitialProps = async ({req}) => {
+  try {
+    console.log("the req header in getinitialprops  : " , req.headers);
+    const { data } = await buildClient(req).get('/api/users/currentuser');
+    return data;  
+  } catch (error) {
+    return error;
   }
-  
-  return {};
 };
 
 export default LandingPage;
