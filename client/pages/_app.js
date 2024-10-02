f46b19c68468cac9962b0c9236447802e1b32ed1
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-10-02 22:05:48 +0530

    made the header component which shows different options depending on wheather the user is signed in or not

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps , currentUser}) => {
   return (
     <div>
-      <h3>{currentUser?.email}</h3>
+      <Header currentUser={currentUser}></Header>
       <Component {...pageProps}  />
     </div>
   );
