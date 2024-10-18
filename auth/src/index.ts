c18e60ec6f2e900f07d58705005e250bd904b4da
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-10-18 22:29:51 +0530

    wrote mongo uri to acces the mongo url

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  if(!process.env.MONGO_URI){
+    throw new Error("MONGO URI must be defined")
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.error(err);
   }
