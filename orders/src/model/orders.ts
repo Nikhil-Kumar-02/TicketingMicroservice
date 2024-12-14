c4abe134242bfc6066754ea5f3638e920a3e5078
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-12-14 09:48:35 +0530

    created the order model in the order service

---
+++ orders/src/model/orders.ts
@@ -0,0 +1,67 @@
+import mongoose, { mongo, Mongoose } from "mongoose";
+import { OrderStatus } from "@nkticket/common";
+
+//an interface that describes  the properties that are required to create a new order
+interface OrderAttrs{
+  userId: string;
+  status: OrderStatus;
+  expiresAt : Date;
+  ticket : mongoose.Schema.Types.ObjectId;
+}
+
+//an interface that describes the properties that a order document has 
+interface OrderDoc extends mongoose.Document{
+  userId: string;
+  status: OrderStatus;
+  expiresAt : Date;
+  ticket : mongoose.Schema.Types.ObjectId;
+}
+
+//an interface that describes the property that the order model has
+interface OrderModel extends mongoose.Model<OrderDoc>{
+  build(attrs:OrderAttrs):OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId : {
+      type : String,
+      required : true,
+    },
+    status : {
+      type : String,
+      required : true,
+      enum : Object.values(OrderStatus),
+      default : OrderStatus.Created
+    },
+    expiresAt : {
+      type : mongoose.Schema.Types.Date,
+    },
+    ticket : {
+      type : mongoose.Schema.Types.ObjectId,
+      ref : 'Ticket'
+    }
+  },
+  {
+    toJSON: {
+      transform(doc,ret){
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      }
+    }
+  }
+);
+
+orderSchema.statics.build = (arrts:OrderAttrs) => {
+  return new Order(arrts);
+}
+//above alone would have suffice if we were to use js we dont have to write this
+//interface UserModel extends mongoose.Model<any>{
+//   build(attrs:userAttrs):any;
+// }
+
+const Order = mongoose.model<OrderDoc,OrderModel>('Order' , orderSchema);
+
+
+export { Order };
