7f158a703a3b919ba2cbaddbda57fc95d860ea8e
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-12-24 20:51:23 +0530

    created order-created-listener and also modified the ticket model to have a orderId temparory feild

---
+++ tickets/src/events/consumer/order-created-listener.ts
@@ -0,0 +1,46 @@
+import { Listener , OrderCreatedEvent, Subjects } from "@nkticket/common";
+import { Consumer, Kafka } from "kafkajs";
+import { Ticket } from "../../models/ticket";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
+  topicName: Subjects.OrderCreated = Subjects.OrderCreated;
+  groupName = "ticket-service-order-created";
+  consumer: Consumer;
+
+  constructor(kafka: Kafka) {
+    super();
+    this.consumer = kafka.consumer({ groupId: this.groupName });
+  }
+
+  async logReceivedData(data: OrderCreatedEvent["data"], topic: string, partition: number) {
+    console.log("Received OrderCreatedEvent:");
+    console.log("Data:", data);
+    console.log("Topic:", topic);
+    console.log("Partition:", partition);
+
+    const { id:orderId , ticket:{id:ticketId}} = data;
+    
+    //find the ticket that the order is reserving
+    const ticket = await Ticket.findById(ticketId);
+
+    //if no then throw an error
+    if(!ticket){
+      throw new Error("ticket not found in ticket service order created listener")
+    }
+
+    //mark the ticket as being reserved by setting the order id property
+    ticket.set({orderId:orderId})
+
+    //save the ticket
+    await ticket.save();    
+  }
+
+  validateMessage(data: any): data is OrderCreatedEvent["data"] {
+    return (
+      typeof data.id === "string" &&
+      typeof data.title === "string" &&
+      typeof data.price === "number"
+    );
+  }
+
+}
